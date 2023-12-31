82025783573bf477fc5da175c0bda10bcceac4dd
bartek <bartek.banach@proton.me>
2023-12-31 02:55:36 +0100

    Styles fix.

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar/Navbar';
+import styles from './Layout.module.css';
+
 
 function Layout() {
   return (
-    <>
+    <div className={styles.container}>
       <Navbar />
       <Outlet />
-    </>
+    </div>
   );
 }
 
